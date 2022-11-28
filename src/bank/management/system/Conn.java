f8d88364436a938e47fd03dccb39c0a0417b2796
whyramkrishnakhan <ramkrishna.khan@msds.christuniversity.in>
2022-11-28 08:51:12 +0530

    first commit

---
+++ src/bank/management/system/Conn.java
@@ -0,0 +1,20 @@
+package bank.management.system;
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","12345678");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
